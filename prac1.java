f1e8654b6c8ed909571b5a673820ebe79a90c72f
L Jaswanthi <106381737+Jaswanthilokku1323@users.noreply.github.com>
2022-06-22 13:34:05 +0530

    Add files via upload

--- prac1.java
+++ prac1.java
@@ -1,10 +1,15 @@
-package java;
+package pro3;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 public class prac1 {
 	public static void main(String[] args) {
-	
-		
-	}
+    	String path1="D:\\chromedriver.exe";
+    	System.setProperty("webdriver.chrome.driver",path1);
+    	WebDriver driver=new ChromeDriver();
+    	driver.get("https://adactinhotelapp.com/");
+    	driver.close();
+    }
 }
+
